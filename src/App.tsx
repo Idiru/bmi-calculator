446f9a016f076ea14e3eff473367c1da99f5935d
Idir FOUQUET <fouquetidir@gmail.com>
2025-02-25 18:26:25 +0100

    Section 2 mobile done

--- src/App.tsx
+++ src/App.tsx
@@ -17,6 +17,16 @@ function App() {
           <Calculator />
         </div>
       </div>
+      <div className='container-section-2'>
+        <img src="/sushi-man.png" alt="sushi" />
+        <div className='section-2-content'>
+          <h2>What your BMI result means</h2>
+          <p>A BMI range of 18.5 to 24.9 is considered a 'healthy weight.' Maintaining a healthy weight may lower your chances of experiencing health issues later on, such as obesity and type 2 diabetes. Aim for a nutritious diet with reduced fat and sugar content, incorporating ample fruits and vegetables. Additionally, strive for regular physical activity, ideally about 30 minutes daily for five days a week.</p>
+        </div>
+      </div>
+      <div className='container-section-3'>
+        
+      </div>
     </>
   )
 }
