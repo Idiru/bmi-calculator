b6134f92198252fdc63ec1480d449f674a3f5415
Idir FOUQUET <fouquetidir@gmail.com>
2025-02-27 17:17:38 +0100

    Section 2 done

--- src/App.tsx
+++ src/App.tsx
@@ -34,31 +34,31 @@ function App() {
       alt: "gender",
       title: "Gender",
       text: "The development and body fat composition of girls and boys vary with age. Consequently, a child's age and gender are considered when evaluating their BMI."
-    }, 
+    },
     {
       img: "/cake.svg",
       alt: "cake",
       title: "Age",
       text: "In aging individuals, increased body fat and muscle loss may cause BMI to underestimate body fat content."
-    }, 
+    },
     {
       img: "/muscle.svg",
       alt: "muscle",
       title: "Muscle",
       text: "BMI may misclassify muscular individuals as overweight or obese, as it doesn't differentiate muscle from fat."
-    }, 
+    },
     {
       img: "/baby.svg",
       alt: "baby",
       title: "Pregnancy",
       text: "Expectant mothers experience weight gain due to their growing baby. Maintaining a healthy pre-pregnancy BMI is advisable to minimise health risks for both mother and child."
-    }, 
+    },
     {
       img: "/human.svg",
       alt: "human",
       title: "Race",
       text: "Certain health concerns may affect individuals of some Black and Asian origins at lower BMIs than others. To learn more, it is advised to discuss this with your GP or practice nurse."
-    }, 
+    },
   ]
 
   return (
@@ -75,10 +75,12 @@ function App() {
         </div>
       </div>
       <div className='container-section-2'>
-        <img src="/sushi-man.png" alt="sushi" />
-        <div className='section-2-content'>
-          <h2>What your BMI result means</h2>
-          <p>A BMI range of 18.5 to 24.9 is considered a 'healthy weight.' Maintaining a healthy weight may lower your chances of experiencing health issues later on, such as obesity and type 2 diabetes. Aim for a nutritious diet with reduced fat and sugar content, incorporating ample fruits and vegetables. Additionally, strive for regular physical activity, ideally about 30 minutes daily for five days a week.</p>
+        <div className='container-items'>
+          <img src="/sushi-man.png" alt="sushi" />
+          <div className='section-2-content'>
+            <h2>What your BMI result means</h2>
+            <p>A BMI range of 18.5 to 24.9 is considered a 'healthy weight.' Maintaining a healthy weight may lower your chances of experiencing health issues later on, such as obesity and type 2 diabetes. Aim for a nutritious diet with reduced fat and sugar content, incorporating ample fruits and vegetables. Additionally, strive for regular physical activity, ideally about 30 minutes daily for five days a week.</p>
+          </div>
         </div>
       </div>
       <div className='container-section-3'>
@@ -89,15 +91,15 @@ function App() {
         }
       </div>
       <div className='container-section-4'>
-          <h2>Limitations of BMI</h2>
-          <p className='section-4-text'>Although BMI is often a practical indicator of healthy weight, it is not suited for every person. Specific groups should carefully consider their BMI outcomes, and in certain cases, the measurement may not be beneficial to use.</p>
-          <div className='container-limitations'>
-            {
-              limitations.map((limitation) => {
-                return <Limitation img={limitation.img} alt={limitation.alt} title={limitation.title} text={limitation.text}/>
-              })
-            }
-          </div>
+        <h2>Limitations of BMI</h2>
+        <p className='section-4-text'>Although BMI is often a practical indicator of healthy weight, it is not suited for every person. Specific groups should carefully consider their BMI outcomes, and in certain cases, the measurement may not be beneficial to use.</p>
+        <div className='container-limitations'>
+          {
+            limitations.map((limitation) => {
+              return <Limitation img={limitation.img} alt={limitation.alt} title={limitation.title} text={limitation.text} />
+            })
+          }
+        </div>
       </div>
     </>
   )
